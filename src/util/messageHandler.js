061ce854802c67a8c7c090d45fbbd91a4edd0059
rhicu <pheonix90@gmx.de>
2018-04-08 10:46:18 +0200

    finished updatePrintedRaids() functionality

--- src/util/messageHandler.js
+++ src/util/messageHandler.js
@@ -17,9 +17,23 @@ class MessageHandler {
             const messageArray = results[1]
             let counter = 0
 
-            messageArray.forEach((message) => {
-                if(message.id)
-            })
+            for(let index = 0; index < messageArray.length; index++) {
+                const message = messageArray[index]
+                if(raidArray[index] && raidArray[index].messageID === message.id) {
+                    MessageHandler._updatePrintedRaid(message, raidArray[index])
+                } else if(raidArray[index] && raidArray[index].messageID !== message.id) {
+                    counter = index
+                    break
+                }
+            }
+
+            for(let index = counter; index < messageArray.length; index++) {
+                messageArray[index].delete()
+            }
+
+            for(let index = counter; index < raidArray.length; index++) {
+                MessageHandler._printRaid(raidArray[index], bot)
+            }
         }).catch((error) => {
             throw error
         })
@@ -42,6 +56,34 @@ class MessageHandler {
      */
     static _getRaidPlannerChannel(bot) {
         return bot.guilds.find('id', config.serverID).channels.find('id', config.raidPlannerChannelID)
+    }
+
+    /**
+     * @private
+     * @param {Message} message
+     * @param {Raid} raid
+     */
+    static _updatePrintedRaid(message, raid) {
+        raid.generateEmbed()
+            .then((embed) => {
+                message.edit({embed})
+            })
+    }
+
+    /**
+     * @private
+     * @param {Raid} raid
+     * @param {Client} bot
+     */
+    static _printRaid(raid, bot) {
+        const embed = raid.generateEmbed()
+        const channel = MessageHandler._getRaidPlannerChannel()
+
+        channel.send({embed})
+            .then((message) => {
+                raid.messageID = message.id
+                bot.database.addOrUpdateRaid(raid)
+            })
     }
 }
 
