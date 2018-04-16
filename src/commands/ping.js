7f57669b9657583a3e70a8da3c9db22db9013aa0
rhicu <test@example.com>
2018-04-16 11:21:54 +0200

    refactored ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,19 +1,19 @@
 exports.run = (bot, msg) => {
-    msg.channel.send("Ping?")
-        .then(msg => {
-            msg.edit(`Pong! (took: ${msg.createdTimestamp - msg.createdTimestamp}ms)`);
-        });
-};
-  
+    msg.channel.send('Ping?')
+        .then((msg) => {
+            msg.edit(`Pong! (took: ${msg.createdTimestamp - msg.createdTimestamp}ms)`)
+        })
+}
+
 exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: [],
     permLevel: 0
-};
-  
+}
+
 exports.help = {
-    name: "ping",
-    description: "Ping/Pong command. I wonder what this does? /sarcasm",
-    usage: "ping"
-};
+    name: 'ping',
+    description: 'Ping/Pong command. I wonder what this does? /sarcasm',
+    usage: 'ping'
+}
