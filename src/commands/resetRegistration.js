e279f17f029732a55599db48438f32c58840b06d
rhicu <test@example.com>
2018-09-09 21:59:11 +0200

    added new command resetRegistration

---
+++ src/commands/resetRegistration.js
@@ -0,0 +1,40 @@
+exports.run = async (bot, msg, args) => {
+    try {
+        if(args.length < 1) {
+            msg.reply('Zu wenige Argumente übergeben. Es muss 1 sein!')
+            return
+        }
+
+        const raidID = parseInt(args[0])
+        const raid = await bot.database.getRaidByID(raidID)
+        if(!raid) {
+            msg.reply('Konnte den Raid nicht finden. Prüfe die Eingabe und versuchs nochmal!')
+            return
+        }
+
+        raid.member.confirmed.forEach((player) => {
+            raid.member.registered.push(player)
+        })
+
+        raid.member.confirmed = []
+
+    } catch(error) {
+        if(msg) {
+            msg.reply(error.message)
+        }
+        console.log(`printRaids:\n${error.stack}`)
+    }
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['reset'],
+    permLevel: 3
+}
+
+exports.help = {
+    name: 'registrierungZurücksetzen',
+    description: 'Setzt alle Registrierungen eines Raids zurück',
+    usage: 'registrierungZurücksetzen <raidID>'
+}
