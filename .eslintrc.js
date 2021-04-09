73d4720c50397ae8d6412b0ade29c91299dec65f
rhicu <rhicu@posteo.de>
2021-04-09 14:16:07 +0200

    starting with v2 and typescript

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['airbnb-typescript'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'jsx-a11y/anchor-is-valid': 0,
+    'import/prefer-default-export': 0
+  },
+  env: {
+    node: true,
+    jest: true,
+  },
+};
--- src/util/eventLoader.js
+++
@@ -1,8 +0,0 @@
-const reqEvent = (event) => require(`../events/${event}`)
-
-/**
- * @param {Client} client
- */
-module.exports = (client) => {
-    client.on('message', reqEvent('message'))
-}
